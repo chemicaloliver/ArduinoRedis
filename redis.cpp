e6acbace04ba97a80da8879342252334d5b59763
Oliver Smith <chemicaloli@gmail.com>
2011-09-04 19:10:19 +0100

    Added connect functionality

--- redis.cpp
+++ redis.cpp
@@ -6,16 +6,60 @@
  */
 
 #include <redis.h>
-#include <SPI.h>
-#include <Ethernet.h>
+#include <Client.h>
 
 
-ArduinoRedisClient::ArduinoRedisClient()
+ArduinoRedisClient::ArduinoRedisClient(uint8_t *ip, uint16_t port) : _client(ip,port) 
 {
     
 }
 
-bool ArduinoRedisClient::begin()
+bool ArduinoRedisClient::connect()
 {
-    
+    _client.connect();
 }
+
+/**
+ * Set a key value pair
+ * 
+ * @param 
+ * @param 
+ * @return 
+ */
+
+bool ArduinoRedisClient::set(char* key, char* value)
+{
+
+}
+
+/**
+ * Publish a message to a pub/sub topic
+ * 
+ * @param char* topic
+ * @param char* message
+ * @return bool send status
+ */
+bool ArduinoRedisClient::publish(char* topic, char* message)
+{
+
+}
+
+/**
+ * Disconnect from redis
+ * 
+ */
+
+void ArduinoRedisClient::disconnect()
+{
+    _client.println("$4");
+    _client.println("quit");
+}
+
+bool ArduinoRedisClient::connected()
+{
+    bool rc = (bool)_client.connected();
+    if (!rc) _client.stop();
+    return rc;  
+}
+//
+//
--- redis.h
+++ redis.h
@@ -6,14 +6,20 @@
  */
 
 #include <WProgram.h>;
+#include <Client.h>
 
 class ArduinoRedisClient {
 
 private:
+    Client _client;
     
 public:
-    ArduinoRedisClient();
-    bool begin();
+    ArduinoRedisClient(uint8_t *, uint16_t);
+    bool connect();
+    
+    bool set(char *, char *);
+    bool publish(char *, char *);
+    
+    bool connected();
+    void disconnect();
 };
-
-extern Client client; //the network client
